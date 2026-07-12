fe3376d995bc27f6f59efd8ea59c44fbb538a1ae
learnjava0 <kdinesh72453@gmail.com>
2026-07-12 14:15:58 +0530

    chore: Remove backend and target folders from frontend branch

--- backend/src/main/java/com/hackathon/transitops/entity/DriverStatus.java
+++
@@ -1,8 +0,0 @@
-package com.hackathon.transitops.entity;
-
-public enum DriverStatus {
-    AVAILABLE,
-    ON_TRIP,
-    OFF_DUTY,
-    SUSPENDED
-}
--- backend/src/main/java/com/hackathon/transitops/entity/TripStatus.java
+++
@@ -1,8 +0,0 @@
-package com.hackathon.transitops.entity;
-
-public enum TripStatus {
-    DRAFT,
-    DISPATCHED,
-    COMPLETED,
-    CANCELLED
-}
